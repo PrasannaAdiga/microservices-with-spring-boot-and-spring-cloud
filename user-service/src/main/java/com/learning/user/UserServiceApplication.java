6b2f964e5759d655ad2107132ad0a9fc9341b2cb
Prasanna Adiga <gpadiga.adiga@gmail.com>
2018-05-20 07:27:44 +0530

    Creating spring cloud service discovery

--- user-service/src/main/java/com/learning/user/UserServiceApplication.java
+++ user-service/src/main/java/com/learning/user/UserServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -19,6 +20,7 @@ import com.learning.rest.service.UserService;
 @ComponentScan(basePackages = "com.learning.*")
 @EntityScan(basePackages = "com.learning.api.entity")
 @EnableJpaRepositories("com.learning.api.repository")
+@EnableEurekaClient
 public class UserServiceApplication implements CommandLineRunner {
 
 	@Autowired
